ed9375e592413d6cdca8bdb172685699d71c99bb
Mauricio <mauricio.vs93@gmail.com>
2020-05-02 13:32:00 -0300

    Adicionado api.

---
+++ api/API/API/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class User : BaseEntity
+    {       
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public User()
+        {
+
+        }
+        public User(string userName, string password, string role)
+        {
+            UserName = userName;
+            Password = password;
+            Role = role;
+        }
+    }
+}
