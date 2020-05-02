40f789284bede44af9b06a2b938ba29298f656f2
Lucas <lucas.bertolo@gatec.com.br>
2020-05-02 13:05:08 -0300

    SideList

--- mobile/MegaHackAmericanas/src/views/Products/MainProduct/ProductActions/ProductActions.js
+++ mobile/MegaHackAmericanas/src/views/Products/MainProduct/ProductActions/ProductActions.js
@@ -1,62 +1,19 @@
 import React, {useContext} from 'react';
-import styled from 'styled-components';
 import comments from '../../../../assets/icons/comments.png';
 import grouplist from '../../../../assets/icons/grouplist.png';
 import info from '../../../../assets/icons/info.png';
 import {ThemeContext} from '../../../../theme/themesContext';
-
-const ContainerActions = styled.View`
-  flex-direction: column;
-  height: 200px;
-  width: 100%;
-`;
-
-const SectionIcons = styled.View`
-  flex: 0.6;
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 0 30px;
-`;
-
-export const Icon = styled.Image`
-  width: 64px;
-  height: 64px;
-`;
-
-export const IconContent = styled.View`
-  width: 35%;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const TouchableIcon = styled.TouchableOpacity``;
-
-export const IconText = styled.Text`
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  color: ${props => props.theme.onSurfaceSecondary};
-`;
-
-const SectionButton = styled.View`
-  justify-content: center;
-  align-items: center;
-  padding: 30px;
-  margin-top: 30px;
-`;
-
-export const MainButton = styled.TouchableOpacity`
-  background-color: ${props => props.theme.primary};
-  width: 211px;
-  height: 37px;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const MainButtonText = styled.Text`
-  color: ${props => props.theme.background};
-`;
+import {
+  ContainerActions,
+  Icon,
+  IconContent,
+  IconText,
+  MainButton,
+  MainButtonText,
+  SectionButton,
+  SectionIcons,
+  TouchableIcon,
+} from './styles';
 
 export function ProductActions() {
   const {theme} = useContext(ThemeContext);
